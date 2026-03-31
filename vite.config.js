4c867aad22e353e4db6bd4fe74b3bce8f1c14c98
Illumye <aseguy4978@gmail.com>
2026-03-31 21:41:23 +0200

    feat: /download

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwincss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     plugins: [
         tailwincss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main:     resolve(__dirname, 'index.html'),
+                download: resolve(__dirname, 'download.html'),
+            },
+        },
+    },
 })
